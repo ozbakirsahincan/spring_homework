b31b674a051bd521afc09c03e7cc1f35fd7a89c4
ozbakirsahincan <ozbakirsahincan@gmail.com>
2024-03-24 11:20:04 +0100

    Bussiness abstract servisine todo lar eklendi.

--- src/main/java/com/example/Kodlama/io/devs/business/abstracts/LanguageService.java
+++ src/main/java/com/example/Kodlama/io/devs/business/abstracts/LanguageService.java
@@ -2,6 +2,10 @@ package com.example.Kodlama.io.devs.business.abstracts;
 import java.util.List;
 
 import com.example.Kodlama.io.devs.entities.concretes.ProgramingLanguage;
+
 public interface LanguageService {
     List<ProgramingLanguage> getAll();
+    // TODO : getById Methodu eklenecek
+    // TODO : deleteById Methodu eklenecek
 }
+
